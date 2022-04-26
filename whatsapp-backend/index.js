8d03020d4bef8bfec2a7e49bac1dc26d19e4ed67
Srishti Singh <88429504+srishti20409@users.noreply.github.com>
2022-04-27 03:57:30 +0530

    login
    login

--- whatsapp-backend/index.js
+++ whatsapp-backend/index.js
@@ -1,35 +1,102 @@
-const express = require('express')
-const app = express()
-const db = require("./whatsapp-backend/models");
-db.sequelize.sync();
-db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
-  });
+const express = require("express");
+const mysql = require("mysql");
 const cors = require("cors");
 
-var corsOptions = {
-    origin: "http://localhost:8081"
-  };
-  app.use(cors(corsOptions));
-  // parse requests of content-type - application/json
-  app.use(express.json());
-  // parse requests of content-type - application/x-www-form-urlencoded
-  app.use(express.urlencoded({ extended: true }));
-  // simple route
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
-app.get('/',(req,res) => {
-    res.status(200).send('hello world')
-})
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-const port = process.env.PORT || 8080;
+const app = express();
 
-app.listen(port , ()=> console.log(`listening on local host ${port}`))
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(
+  session({
+    key: "userId",
+    secret: "subscribe",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24,
+    },
+  })
+);
 
-const mysql = require('mysql')
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "password",
+  database: "DBMS",
+});
 
+app.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+      console.log(err);
+    }
 
+    db.query(
+      "INSERT INTO users (username, password) VALUES (?,?)",
+      [username, hash],
+      (err, result) => {
+        console.log(err);
+      }
+    );
+  });
+});
 
+app.get("/login", (req, res) => {
+  if (req.session.user) {
+    res.send({ loggedIn: true, user: req.session.user });
+  } else {
+    res.send({ loggedIn: false });
+  }
+});
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
+  db.query(
+    "SELECT * FROM users WHERE username = ?;",
+    username,
+    (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      }
+
+      if (result.length > 0) {
+        bcrypt.compare(password, result[0].password, (error, response) => {
+          if (response) {
+            req.session.user = result;
+            console.log(req.session.user);
+            res.send(result);
+          } else {
+            res.send({ message: "Wrong username/password combination!" });
+          }
+        });
+      } else {
+        res.send({ message: "User doesn't exist" });
+      }
+    }
+  );
+});
+
+app.listen(3001, () => {
+  console.log("running server");
+});
--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Form from "./component/login/Form";
 import {BrowserRouter as Router ,Routes,  Route} from 'react-router-dom'
 import Main from "./component/main/Main.js"
+import Login from "./component/login/login";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path= '/' element={<Main/>}/>
           <Route exact path= '/form' element={<Form/>}/>
+          <Route exact path = '/login' element={<Login/>}/>
         </Routes>
       
       {/* <Route exact path = "/form" component={Form}/> */}
