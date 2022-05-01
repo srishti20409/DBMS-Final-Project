4b54cc1c324653d2f660b7bb90d3cccd0b70007c
Srishti Singh <88429504+srishti20409@users.noreply.github.com>
2022-05-01 09:53:23 +0530

    Update Cell.js

--- whatsapp-mern/src/component/Groups/Cell.js
+++ whatsapp-mern/src/component/Groups/Cell.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import "./style.css";
 
+var arr= [];
+function myFunction(inputCB) {
+    var checkBox = document.getElementById("myCheck");
+    if (checkBox.checked == true){
+        arr.push(inputCB);
+        alert(arr);
+        console.log(arr);
+      } else {
+         console.log = "unchecked";
+      }
+  
+}
+
 function Cell(props) {
     return (
         <div className='cell'>
-            <h2>{props.val.USER_name}</h2>
-            <p>{props.val.USER_phone_number}</p>
+            <input 
+            type="checkbox"
+            id="myCheck" 
+            onClick = {myFunction(props.val.idUSER)}
+            placeholder='props.val.USER_name'/>  
         </div>
     )
 }
